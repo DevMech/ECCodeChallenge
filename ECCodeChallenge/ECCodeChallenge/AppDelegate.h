7b074bb495436b25a73e8c380a51a9a51aafa083
DevMech <agmecham@gmail.com>
2015-08-01 14:26:27 -0600

    Initial Commit

---
+++ ECCodeChallenge/ECCodeChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ECCodeChallenge
+//
+//  Created by Austin Mecham on 8/1/15.
+//  Copyright (c) 2015 Austin Mecham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ECCodeChallenge/ECCodeChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ECCodeChallenge
+//
+//  Created by Austin Mecham on 8/1/15.
+//  Copyright (c) 2015 Austin Mecham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
